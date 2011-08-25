4d24078d82a0271e7c0ee471d25d0116e2ccf729
Scott Wasserman <slw@apprenaissance.com>
2011-08-25 10:42:30 -0400

    Added utilities class for color manipulation.  Includes conversion of UIColor to hex string.

---
+++ Utilities/ARColorHelper.h
@@ -0,0 +1,14 @@
+//
+//  ARColorHelper.h
+//  AllFanz
+//
+//  Created by Scott Wasserman on 8/23/11.
+//  Copyright 2011 appRenaissance LLC. All rights reserved.
+//
+
+
+
+@interface ARColorHelper : NSObject {}
+
++ (NSString *)buildHexColorFromUIColor:(UIColor*)color;
+@end
