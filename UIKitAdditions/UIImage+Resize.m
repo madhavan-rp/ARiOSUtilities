a84b529db79be2dd958101992d7a519bce29eb69
Aaron Kuehler <aaron.kuehler@gmail.com>
2011-09-23 11:50:43 -0400

    Added default fall-through  cases to switches in  UIImage+Resize.

--- UIKitAdditions/UIImage+Resize.m
+++ UIKitAdditions/UIImage+Resize.m
@@ -166,6 +166,7 @@
             transform = CGAffineTransformTranslate(transform, 0, newSize.height);
             transform = CGAffineTransformRotate(transform, -M_PI_2);
             break;
+        default: break;
     }
     
     switch (self.imageOrientation) {
@@ -180,6 +181,7 @@
             transform = CGAffineTransformTranslate(transform, newSize.height, 0);
             transform = CGAffineTransformScale(transform, -1, 1);
             break;
+        default: break;
     }
     
     return transform;
