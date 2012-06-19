5f960b2b84426c9969338635a0fcc21c9de7b305
Adam Duke <adam.v.duke@gmail.com>
2012-06-19 11:39:16 -0400

    format NSDate+Comparison

--- ARFoundationAdditions/NSDate+Comparison.h
+++ ARFoundationAdditions/NSDate+Comparison.h
@@ -1,15 +1,14 @@
-//
-//  NSDate+Comparison.h
-//  This Is Hardcore
-//
-//  Created by Odin on 6/18/12.
-//  Copyright (c) 2012 appRenaissance. All rights reserved.
-//
+/*  NSDate+Comparison.h
+ *
+ *  Created by Odin on 6/18/12.
+ *  Copyright (c) 2012 appRenaissance. All rights reserved.
+ *
+ */
 
 #import <Foundation/Foundation.h>
 
 @interface NSDate (Comparison)
 
-+ (BOOL)date:(NSDate*)date isBetweenDate:(NSDate*)beginDate andDate:(NSDate*)endDate;
++ (BOOL)date:(NSDate *)date isBetweenDate:(NSDate *)beginDate andDate:(NSDate *)endDate;
 
 @end
--- ARFoundationAdditions/NSDate+Comparison.m
+++ ARFoundationAdditions/NSDate+Comparison.m
@@ -1,23 +1,26 @@
-//
-//  NSDate+Comparison.m
-//  This Is Hardcore
-//
-//  Created by Odin on 6/18/12.
-//  Copyright (c) 2012 appRenaissance. All rights reserved.
-//
+/*  NSDate+Comparison.m
+ *
+ *  Created by Odin on 6/18/12.
+ *  Copyright (c) 2012 appRenaissance. All rights reserved.
+ *
+ */
 
 #import "NSDate+Comparison.h"
 
 @implementation NSDate (Comparison)
 
-+ (BOOL)date:(NSDate*)date isBetweenDate:(NSDate*)beginDate andDate:(NSDate*)endDate
++ (BOOL)date:(NSDate *)date isBetweenDate:(NSDate *)beginDate andDate:(NSDate *)endDate
 {
-    if ([date compare:beginDate] == NSOrderedAscending)
+    if([date compare:beginDate] == NSOrderedAscending)
+    {
         return NO;
-    
-    if ([date compare:endDate] == NSOrderedDescending) 
+    }
+    if([date compare:endDate] == NSOrderedDescending)
+    {
         return NO;
-    
+    }
+
     return YES;
 }
+
 @end
