f4081c7d3aab614dc42111436056977e926306da
Kevin Clough <kevinclough@gmail.com>
2012-06-26 09:53:47 -0400

    Added category on UIColor to get UIColor from hex

---
+++ ARUIKitAdditions/UIColor+HexString.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+HexString.h
+//  This Is Hardcore
+//
+//  Created by Kevin Clough on 6/26/12.
+//  Copyright (c) 2012 appRenaissance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HexString)
+
++ (UIColor *) colorWithHexString: (NSString *) hexString;
+
+@end
